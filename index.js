f3f76de7bf52573352ff5859693f512d8059f96a
obieh <o_amadic@yahoo.com>
2024-06-28 10:39:58 +0100

    minikube deployment

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 5000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('A New Beginning');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+
